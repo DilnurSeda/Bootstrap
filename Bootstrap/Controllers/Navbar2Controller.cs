3b10d69cc6643a8f4249d21d3cc438197cd34a12
dilnur seda engin <dilnur.seda@hotmail.com>
2019-04-29 19:42:23 +0300

    Add project files.

---
+++ Bootstrap/Controllers/Navbar2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bootstrap.Controllers
+{
+    public class Navbar2Controller : Controller
+    {
+        // GET: Navbar2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
